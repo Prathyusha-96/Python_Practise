f24bdfa41f935bbfade708f055dc88bb56009d27
Prathyusharam <prathyushaa1996@gmail.com>
2022-11-01 21:21:17 +0530

    add test file

---
+++ Day10/pandas.py
@@ -0,0 +1,6 @@
+#PANDAS
+#IT'S AN open source library in python for data analysis
+#and data manipulation and data visualization
+#firststep
+#import pandas as pd
+
---
+++ Day10/test_practise3.py
@@ -0,0 +1,8 @@
+#FIND THE difference
+s = 'abcd'
+t = 'abcde'
+for i in t:
+    if s.count(i) != t.count(i):
+        print (i)
+
+#fibonacci seriees
